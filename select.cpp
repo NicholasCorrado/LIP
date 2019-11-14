2b5ed26b6f160a2afa030f03ad2e426d0571dd9a
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2019-11-13 19:32:03 -0600

    Added old Select code for String data (commented out)

--- select.cpp
+++ select.cpp
@@ -6,7 +6,50 @@
 #include "select.h"
 #include "util/util.h"
 
-// Select on a column with string data
+std::shared_ptr<arrow::Table> Select(std::shared_ptr<arrow::Table> table, std::string select_field, std::shared_ptr<arrow::Scalar> value, arrow::compute::CompareOperator op) {
+
+    arrow::Status status;
+    std::shared_ptr<arrow::RecordBatch> in_batch;
+
+    std::vector<std::shared_ptr<arrow::ArrayData>> out_arrays;      // vector of arrays corresponding to outputted columns in a given batch
+    std::unique_ptr<arrow::RecordBatchBuilder> out_batch_builder;   // to build a RecordBatch from a vector of arrays
+    std::vector<std::shared_ptr<arrow::RecordBatch>> out_batches;   // output table will be built from a vector of RecordBatches
+
+    status = arrow::RecordBatchBuilder::Make(table->schema(), arrow::default_memory_pool(), &out_batch_builder);
+    EvaluateStatus(status);
+
+    // Instantiate things needed for a call to Compare()
+    arrow::compute::FunctionContext function_context(arrow::default_memory_pool());
+    arrow::compute::CompareOptions compare_options(op);
+    auto* filter = new arrow::compute::Datum();
+
+    auto* reader = new arrow::TableBatchReader(*table);
+
+    while (reader->ReadNext(&in_batch).ok() && in_batch != nullptr) {
+
+        status = arrow::compute::Compare(&function_context, in_batch->GetColumnByName(select_field), value, compare_options, filter);
+
+        auto* out = new arrow::compute::Datum();
+
+        // For each column, store all values that pass the filter
+        for (int i=0; i<in_batch->schema()->num_fields(); i++) {
+            status = arrow::compute::Filter(&function_context, in_batch->column(i), *filter, out);
+            out_arrays.push_back(out->array());
+        }
+
+        auto out_batch = arrow::RecordBatch::Make(in_batch->schema(),out_arrays[0]->length,out_arrays);
+        out_batches.push_back(out_batch);
+    }
+
+    std::shared_ptr<arrow::Table> result_table;
+    status = arrow::Table::FromRecordBatches(out_batches, &result_table);
+    EvaluateStatus(status);
+
+    return result_table;
+}
+
+// Arrow currently does not support vectorized string comparison. Nevertheless, we can still use this function to run
+// select queries on columns with string data if we really wanted to.
 /*
 std::shared_ptr<arrow::Table> SelectString(std::shared_ptr<arrow::Table> table, std::string select_field, std::string value, Operator op) {
 
@@ -50,44 +93,3 @@ std::shared_ptr<arrow::Table> SelectString(std::shared_ptr<arrow::Table> table, 
     return result_table;
 }
 */
-std::shared_ptr<arrow::Table> Select(std::shared_ptr<arrow::Table> table, std::string select_field, std::shared_ptr<arrow::Scalar> value, arrow::compute::CompareOperator op) {
-
-    arrow::Status status;
-    std::shared_ptr<arrow::RecordBatch> in_batch;
-
-    std::vector<std::shared_ptr<arrow::ArrayData>> out_arrays;      // vector of arrays corresponding to outputted columns in a given batch
-    std::unique_ptr<arrow::RecordBatchBuilder> out_batch_builder;   // to build a RecordBatch from a vector of arrays
-    std::vector<std::shared_ptr<arrow::RecordBatch>> out_batches;   // output table will be built from a vector of RecordBatches
-
-    status = arrow::RecordBatchBuilder::Make(table->schema(), arrow::default_memory_pool(), &out_batch_builder);
-    EvaluateStatus(status);
-
-    // Instantiate things needed for a call to Compare()
-    arrow::compute::FunctionContext function_context(arrow::default_memory_pool());
-    arrow::compute::CompareOptions compare_options(op);
-    auto* filter = new arrow::compute::Datum();
-
-    auto* reader = new arrow::TableBatchReader(*table);
-
-    while (reader->ReadNext(&in_batch).ok() && in_batch != nullptr) {
-
-        status = arrow::compute::Compare(&function_context, in_batch->GetColumnByName(select_field), value, compare_options, filter);
-
-        auto* out = new arrow::compute::Datum();
-
-        // For each column, store all values that pass the filter
-        for (int i=0; i<in_batch->schema()->num_fields(); i++) {
-            status = arrow::compute::Filter(&function_context, in_batch->column(i), *filter, out);
-            out_arrays.push_back(out->array());
-        }
-
-        auto out_batch = arrow::RecordBatch::Make(in_batch->schema(),out_arrays[0]->length,out_arrays);
-        out_batches.push_back(out_batch);
-    }
-
-    std::shared_ptr<arrow::Table> result_table;
-    status = arrow::Table::FromRecordBatches(out_batches, &result_table);
-    EvaluateStatus(status);
-
-    return result_table;
-}
