ca3473f06edbaa99ef2059e1d2c52364e6b35faf
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2019-11-15 14:46:43 -0600

    New main and Queries files

---
+++ Queries.h
@@ -0,0 +1,19 @@
+//
+// Created by Nicholas Corrado on 11/15/19.
+//
+
+#ifndef CSV_TO_ARROW_CPP_QUERIES_H
+#define CSV_TO_ARROW_CPP_QUERIES_H
+
+#include <iostream>
+#include <map>
+
+#include "BuildFilter.h"
+#include "Join.h"
+#include "util/util.h"
+#include "JoinExecutor.h"
+
+int main_nick();
+int main_xiating();
+
+#endif //CSV_TO_ARROW_CPP_QUERIES_H
